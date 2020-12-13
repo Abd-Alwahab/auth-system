817412ccaf8db4c544cb15fbf3acdae0852b6524
abdalwahab <bayanadam10@gmail.com>
2020-12-13 16:58:20 +0300

    adding the login

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const { signup, getUsers } = require("./../controllers/auth");
+const { signup, getUsers, login } = require("./../controllers/auth");
 
 router.post("/signup", signup);
+router.post("/login", login);
 router.get("/", getUsers);
 
 module.exports = router;
