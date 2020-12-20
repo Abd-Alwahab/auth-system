202cbfd922a7f3314657511189449adf96225d5e
abdalwahab <bayanadam10@gmail.com>
2020-12-20 15:33:29 +0300

    addding some security packages

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const authRouter = require("./routes/users");
+const rateLimit = require("express-rate-limit");
+const helemt = require("helmet");
+const mongoSnatize = require("express-mongo-sanitize");
+const xssClean = require("xss-clean");
+const hpp = require("hpp");
 require("dotenv").config();
 
 const app = express();
-app.use(express.json());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too Many Request, Fuak Off Man!",
+});
 
+app.use(helemt());
+app.use(mongoSnatize());
+app.use(xssClean());
+app.use(hpp());
+
+app.use("/api", limiter);
+app.use(
+  express.json({
+    limit: "10kb",
+  })
+);
 app.use("/api/v1/users", authRouter);
 
 // const port = process.env.PORT || 8080;
