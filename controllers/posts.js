9c17c28648a62c8b41e707982bd6682de46065d2
abdalwahab <bayanadam10@gmail.com>
2021-01-11 21:46:46 +0300

    adding the post route

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,6 @@
 const catchAsync = require("./../utils/catchAsync");
 const { Post } = require("./../models/postModal");
+const { uploadFromBuffer } = require("../utils/imageHandle");
 
 const getPosts = catchAsync(async (req, res, next) => {
   const posts = await Post.find();
@@ -27,6 +28,38 @@ const getPostDetails = catchAsync(async (req, res, next) => {
 
 const createPost = catchAsync(async (req, res, next) => {
   const { title, description } = req.body;
+
+  if (!title || !req.file)
+    res.status(400).json({
+      status: "fail",
+      data: "please providea post title! and a post photo",
+    });
+
+  let postPhoto = {
+    secure_url: null,
+    public_id: null,
+  };
+
+  let result;
+
+  if (req.file) {
+    postPhoto = req.file.processedImage;
+    result = await uploadFromBuffer(postPhoto.data);
+  }
+
+  const post = new Post({
+    title,
+    description,
+    phot: {
+      public_id: result.public_id,
+      secure_url: result.secure_url,
+    },
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: post,
+  });
 });
 
 const editPost = catchAsync(async (req, res, next) => {
