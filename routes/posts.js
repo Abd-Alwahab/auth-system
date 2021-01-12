6bab14cca03da2e9aeac95dc7333f8ae83480112
abdalwahab <bayanadam10@gmail.com>
2021-01-12 19:07:05 +0300

    working on the edit post route

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 const { resizePostPhoto, upload } = require("./../utils/imageHandle");
 
+const { protect, restricPermissions } = require("./../controllers/auth");
+
 const {
   getPosts,
   getPostDetails,
@@ -11,9 +13,21 @@ const {
 } = require("./../controllers/posts");
 
 router.get("/", getPosts);
-router.post("/", [upload.single("photo"), resizePostPhoto], createPost);
+router.post(
+  "/",
+  protect,
+  restricPermissions("admin"),
+  [upload.single("photo"), resizePostPhoto],
+  createPost
+);
 router.get("/:id", getPostDetails);
-router.patch("/:id", editPost);
-router.delete("/:id", deletePost);
+router.patch(
+  "/:id",
+  protect,
+  restricPermissions("admin"),
+  [upload.single("photo"), resizePostPhoto],
+  editPost
+);
+router.delete("/:id", protect, restricPermissions("admin"), deletePost);
 
 module.exports = router;
